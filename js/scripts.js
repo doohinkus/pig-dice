380a944a57283f2341fa0cddb537fb4f63005ed3
doohinkus <doohinkus@gmail.com>
2016-11-04 21:53:47 -0700

    add referee object to oversee the game.

--- js/scripts.js
+++ js/scripts.js
@@ -1,57 +1,69 @@
-function Player (mid){
-    this.mid = mid;
-    this.score = 0;
-    this.runningTotal = 0;
-    this.dice = 0;
-    this.turn = 0;
+function Player (playerID){
+  this.score = 0;
+  this.runningTotal = 0;
+  this.dice = 0;
+  this.turn = 0;
+  this.playerID = playerID;
 }
+function Referee (){
+  this.players = [];
+  this.winner = "";
+}
+//players object?
 
 Player.prototype.throw = function (){
   var result = Math.floor((Math.random() * 6) + 1);
-  // this.calculateRunningTotal(); placing here adds delay of one press
-  if (this.turn === 1 && result !=1){
-
-
-    return this.dice = result;
-  }else if (result === 1 && this.turn === 1){
-    this.turn = 0;
-    this.runningTotal = 0;
-    return this.dice = 1;
-  }
+  return  result;
 }
 
 Player.prototype.hold = function (){
-  if (this.turn === 1){
-    this.score += this.runningTotal;
-  }
-  return this.turn = 0;
+  this.turn = 0;
+  this.total += this.runningTotal;
+  return this.total;
 }
 
-Player.prototype.calculateRunningTotal = function (){
-  if (this.dice != 1 && this.turn != 0){
-    return this.runningTotal += this.dice;
-  }else if (this.dice === 1){
-    return this.runningTotal = 0;
+Referee.prototype.pickPlayer = function (){
+  this.players[0].turn = 1;
+}
+
+Referee.prototype.calculateRunningTotal = function (player){
+  if (player.dice !=1 && player.turn === 1){
+    player.runningTotal += player.dice;
+  }else if (player.dice === 1){
+    player.runningTotal = 0;
+  }
+  if (player.runningTotal >=100){
+    this.winner = player.ID;
+  }
+  return player.runningTotal;
+}
+
+Referee.prototype.switchPlayers = function (playera, playerb){
+  if (playera.turn === 1){
+    playera.turn = 0;
+    playerb.turn = 1;
+  }else{
+    playerb.turn = 0;
+    playera.turn = 1;
   }
 }
 
 
 $(document).ready(function (){
-  var player1 = new Player (1);
-  var player2 = new Player (2);
-  player1.turn = 1;
+  var player1 = new Player ();
+  var player2 = new Player ();
+  var jimmyTheReferee = new Referee ();
+
+  jimmyTheReferee.players.push(player1, player2);
+
 
 
   $("#roll").click(function (){
-    $("#dice").text(player1.throw());
-    // took away this
-    player1.calculateRunningTotal();
-    $("#player1runningTotal").text(player1.runningTotal);
-    $("#player1score").text(player1.score);
+
   });
+
   //
   $("#hold").click(function (){
-    player1.hold();
-    $("#player1score").text(player1.score);
+
   });
 });
