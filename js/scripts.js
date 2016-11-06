70ba6e2dce9654bbd887db87253e23941275b1f9
doohinkus <doohinkus@gmail.com>
2016-11-06 15:12:51 -0800

    add styles to page and highlight the current player.

--- js/scripts.js
+++ js/scripts.js
@@ -18,9 +18,6 @@ Referee.prototype.throw = function (){
   if(this.gameover === 0){
     this.dice = result;
   }
-
-
-
   if (this.players[0].turn === 1) {
     if (result !=1){
       this.players[0].runningTotal += result;
@@ -58,22 +55,28 @@ Referee.prototype.pickPlayer = function (){
   this.players[0].turn = 1;
 }
 Referee.prototype.checkGame = function (){
-
   if (this.players[0].score >= 100){
     this.players[1].turn = 0;
     this.players[0].turn = 0;
-    this.winner = this.players[0];
+    this.winner = "Player 1 ";;
     this.gameover = 1;
   }else if (this.players[1].score >= 100){
     this.players[1].turn = 0;
     this.players[0].turn = 0;
-    this.winner = this.players[1];
+    this.winner = "Player 2 ";
     this.gameover = 1;
   }
-
 }
 
-
+function switchClass(player1, player2){
+  if (player1.turn === 1){
+    $("div.player1").addClass("highlight");
+    $("div.player2").removeClass("highlight");
+  }else if (player2.turn === 1){
+    $("div.player1").removeClass("highlight");
+    $("div.player2").addClass("highlight");
+  }
+}
 
 
 
@@ -83,9 +86,6 @@ $(document).ready(function (){
   var jimmyTheReferee = new Referee ();
   jimmyTheReferee.players.push(player1, player2);
   jimmyTheReferee.pickPlayer();
-
-
-
 
 
   $("#roll").click(function (){
@@ -100,12 +100,17 @@ $(document).ready(function (){
 
     $("#player1Total").text(player1.score);
     $("#player2Total").text(player2.score);
-    // $("#winner").text("Player " + jimmyTheReferee.winner.playerID + "wins!!!");
+    switchClass(player1, player2);
+
+    if (jimmyTheReferee.gameover === 1){
+      $("#winner").show().text(jimmyTheReferee.winner + "wins!!!");
+    }
+
 
   });
 
-  //
   $("#hold").click(function (){
     jimmyTheReferee.hold();
+    switchClass(player1, player2);
   });
 });
